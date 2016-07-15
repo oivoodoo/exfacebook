77f2e18e2c458d284b0b8278bedb9f5a26b1d3c7
Alexandr Korsak <alex.korsak@gmail.com>
2016-07-15 21:12:18 +0300

    Move http to http.ex

--- lib/exfacebook.ex
+++ lib/exfacebook.ex
@@ -1,24 +1,4 @@
-defmodule Exfacebook.Http do
-  require Poison
+defmodule Exfacebook do
 
-  alias HTTPoison.Response
-  alias HTTPoison.Error
-
-  @http_options [recv_timeout: :infinity, timeout: 10000, hackney: [timeout: 10000, pool: false]]
-
-  def get(url) do
-    case HTTPoison.get(url, [], @http_options) do
-      {:ok, %Response{status_code: 200, body: body}} ->
-        case Poison.decode(body) do
-          {:ok, _value} = state -> state
-          error -> {:error, error}
-        end
-      {:ok, %Response{status_code: 404}} ->
-        {:error, "[Http.get] not found resource"}
-      {:error, %Error{reason: reason}} ->
-        {:error, "[Http.get] #{reason}"}
-      _ ->
-        {:error, "[Http.get] 0xDEADBEEF happened"}
-    end
-  end
+  # TODO: define callbacks for API
 end
