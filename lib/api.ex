116b3b4cb0a4913a3d9742a3cffe7e08b23bfcf2
Alexandr Korsak <alex.korsak@gmail.com>
2016-07-19 01:12:33 +0300

    Add pagination next/prev for get_connections requests

--- lib/api.ex
+++ lib/api.ex
@@ -6,35 +6,54 @@ defmodule Exfacebook.Api do
   alias HTTPoison.Error
 
   defmodule Params do
-    defstruct limit: 25, access_token: nil, fields: "", appsecret_proof: nil
+    defstruct limit: 25, access_token: nil, fields: ""
   end
 
-  def get_connections(object_id, object_name, params) do
+
+  @doc """
+  Use get_connections to read feed, home collections.
+  """
+  def get_connections(params, object_id, object_name) do
     _request(object_name, object_id, params)
   end
 
-  def get_object(object_id, params) do
+
+  def next_page({:error, _error} = state), do: state
+  def next_page({:ok, %{"paging" => %{"next" => url}}}), do: _request(url)
+  def next_page({:ok, _response}), do: {:ok, %{"data" => []}}
+
+
+  def prev_page({:error, _error} = state), do: state
+  def prev_page({:ok, %{"paging" => %{"previous" => url}}}), do: _request(url)
+  def prev_page({:ok, _response}), do: {:ok, %{"data" => []}}
+
+
+  def get_object(params, object_id) do
     _request(object_id, params)
   end
 
+
   defp _request(object_id, params) do
-    _make_url(object_id, params)
-    |> _request
+    url = _make_url(object_id, params)
+    _request(url)
   end
   defp _request(object_name, object_id, params), do: _request(~s(#{object_id}/#{object_name}), params)
-
   defp _request(url), do: Http.get(url)
 
-  defp _make_url(path, %Params{access_token: access_token} = params) do
-    params = params
-    |> Map.put(:appsecret_proof, _encrypt(access_token))
-    |> Map.delete(:__struct__)
-    |> Map.to_list
+
+  def auth(%Params{access_token: access_token} = params, secret) do
+    Map.put(params, :appsecret_proof, _encrypt(secret, access_token))
+  end
+
+
+  defp _make_url(path, %Params{access_token: _access_token} = params) do
+    params = params |> Map.delete(:__struct__) |> Map.to_list
     :hackney_url.make_url("https://graph.facebook.com", "#{@api_version}/#{path}", params)
   end
 
-  defp _encrypt(token) do
-    :crypto.hmac(:sha256, @app_secret, token)
+
+  defp _encrypt(secret, token) do
+    :crypto.hmac(:sha256, secret, token)
     |> Base.encode16(case: :lower)
   end
 end
