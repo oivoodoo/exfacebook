b190ca14aa165720f74e27be8e054313353de5b2
Alexandr Korsak <alex.korsak@gmail.com>
2016-07-21 01:04:01 +0300

    Add test and example for GenServer of exfacebook

--- lib/exfacebook.ex
+++ lib/exfacebook.ex
@@ -9,14 +9,22 @@ defmodule Exfacebook do
 
     * `Exfacebook.Config` - specify `api_version` and http requests for hackney.
 
-  Configuration example:
+  Configuration example(optional variables):
 
       config :exfacebook,
         api_version: "v2.6",
-        http_options: [recv_timeout: :infinity]
+        http_options: [recv_timeout: :infinity],
+        id: "your_app_id_optional",
+        secret: "your_app_secret_optiona"
   """
 
   def start_link(options \\ []) do
     GenServer.start_link(__MODULE__, [], options)
+  end
+
+  alias Exfacebook.Api
+
+  def handle_call({:get_object, id, params}, _from, state) do
+    {:reply, Api.get_object(id, params), state}
   end
 end
---
+++ test/exfacebook_test.exs
@@ -0,0 +1,24 @@
+defmodule ExfacebookTest do
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  alias Exfacebook.Api
+  alias Exfacebook.Params
+
+  setup_all do
+    {:ok, _} = Exfacebook.start_link(name: __MODULE__)
+    ExVCR.Config.cassette_library_dir("fixture/vcr_cassettes")
+    :ok
+  end
+
+  @access_token "EAADGJ4ZBWmDcBALXxlygc6GoIubjuCsNlLZAd6tPGpYsIppJ1TINhgwlg6bfGDyQWK7p0bZA1L12Wx41iEoApQheGPms8eIm1rT4w6htIcEzcl2aaF8Dh4G7hu3jJPE9iQJYrwAk71ZBcJ027QmJeXwDbTNNklQjZAkjgoTZBJOOajh5o9Wsmi"
+
+  test "get_object for facebook" do
+    use_cassette "get_object#me_fields_id_name" do
+      params = %Params{access_token: @access_token, fields: "id,name"}
+      {:ok, %{"id" => id, "name" => name}} = GenServer.call(__MODULE__, {:get_object, :me, params})
+      assert id == "127016687734698"
+      assert name == "Richard Alabgiajbgak Thurnman"
+    end
+  end
+end
