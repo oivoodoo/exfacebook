f47dcd2671561a4dfc2071749caaf61cdcb1b100
Alexandr Korsak <alex.korsak@gmail.com>
2016-07-21 12:23:28 +0300

    Add to gen server calls for get_object, next_page, prev_page, get_connections

--- lib/exfacebook.ex
+++ lib/exfacebook.ex
@@ -24,7 +24,24 @@ defmodule Exfacebook do
 
   alias Exfacebook.Api
 
+  def get_object(pid, id, params), do: GenServer.call(pid, {:get_object, id, params})
+  def get_connections(pid, id, name, params), do: GenServer.call(pid, {:get_connections, id, name, params})
+  def next_page(pid, response), do: GenServer.call(pid, {:next_page, response})
+  def prev_page(pid, response), do: GenServer.call(pid, {:prev_page, response})
+
   def handle_call({:get_object, id, params}, _from, state) do
     {:reply, Api.get_object(id, params), state}
+  end
+
+  def handle_call({:get_connections, id, name, params}, _from, state) do
+    {:reply, Api.get_connections(id, name, params), state}
+  end
+
+  def handle_call({:next_page, response}, _from, state) do
+    {:reply, Api.next_page(response), state}
+  end
+
+  def handle_call({:prev_page, response}, _from, state) do
+    {:reply, Api.prev_page(response), state}
   end
 end
--- test/exfacebook_test.exs
+++ test/exfacebook_test.exs
@@ -15,9 +15,17 @@ defmodule ExfacebookTest do
   test "get_object for facebook" do
     use_cassette "get_object#me_fields_id_name" do
       params = %Params{access_token: @access_token, fields: "id,name"}
-      {:ok, %{"id" => id, "name" => name}} = GenServer.call(__MODULE__, {:get_object, :me, params})
+      {:ok, %{"id" => id, "name" => name}} = Exfacebook.get_object(__MODULE__, :me, params)
       assert id == "127016687734698"
       assert name == "Richard Alabgiajbgak Thurnman"
+    end
+  end
+
+  test "get_connections for authenticated user for feed" do
+    use_cassette "get_connections#majesticcasual_fields_id_name" do
+      params = %Params{access_token: @access_token, fields: "id,name"}
+      {:ok, %{"data" => collection}} = Exfacebook.get_connections(__MODULE__, "majesticcasual", :posts, params)
+      assert Enum.count(collection) == 25
     end
   end
 end
