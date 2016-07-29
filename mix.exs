659c9fdd2777cbec5d2c372b09cb5a63fca93e8a
Alexandr Korsak <alex.korsak@gmail.com>
2016-07-29 22:35:55 +0300

    Bump version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exfacebook.Mixfile do
 
   def project do
     [app: :exfacebook,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      description: description,
      package: package,
